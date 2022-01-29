3fc0c9d4805c820e858165ac912cda5fd4dd6b9c
Aleksey Koleznev <fc.fenomen@mail.ru>
2022-01-29 18:59:52 +0700

    debaging backend-code prodaction

--- app.js
+++ app.js
@@ -33,13 +33,13 @@ app.use('/api/data', dataRoute);
 app.use('/api/admin_panel', adminRoute);
 app.use('/api/upload', uploadRoute);
 
-// app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 //
 // app.get('*', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, 'client', 'build'));
 // });
 
-app.use(express.static("client/build"));
+// app.use(express.static("client/build"));
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
