bd5a41c7ff58f32674cd331885a628d0e6149ef0
Aleksey Koleznev <fc.fenomen@mail.ru>
2022-01-29 12:44:32 +0700

    add code prodaction

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-    mongoURI: "mongodb://127.0.0.1:27017/musictherapy",
+    mongoURI: "mongodb://127.0.0.1:27017/",
+    // mongoURI: "mongodb://127.0.0.1:27017/musictherapy",
     // mongoURI: "mongodb+srv://fenomen:fktif228@cluster0.3qpb1.mongodb.net/data?retryWrites=true&w=majority",
     jwt: 'dev-jwt'
 }
