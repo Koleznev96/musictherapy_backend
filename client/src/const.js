36410cfa295a839b0841416f656ea000137a4654
Aleksey Koleznev <fc.fenomen@mail.ru>
2022-01-29 19:02:17 +0700

    debaging backend-code prodaction

--- client/src/const.js
+++ client/src/const.js
@@ -2,7 +2,7 @@
 // export const httpServer = "http://localhost:5000";
 
 // prodaction
-export const httpServer = "http://178.172.165.101:5000";
+export const httpServer = "http://178.172.165.101";
 
 // herocku
 // export const httpServer = "https://elsadchess.herokuapp.com";
