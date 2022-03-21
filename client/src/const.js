6e7fe2f4459bce81a246d68b0bb92b4baa375d87
Aleksey Koleznev <fc.fenomen@mail.ru>
2022-03-21 20:30:49 +0700

    upload v2 prodaction

--- client/src/const.js
+++ client/src/const.js
@@ -2,7 +2,7 @@
 // export const httpServer = "http://localhost:5000";
 
 // prodaction
-// export const httpServer = "https://admin.musictherapy.by";
+export const httpServer = "https://admin.musictherapy.by";
 
 // herocku
-export const httpServer = "https://musictherapy96.herokuapp.com";
+// export const httpServer = "https://musictherapy96.herokuapp.com";
--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,11 @@
 module.exports = {
     // prodaction
-    // mongoURI: "mongodb://127.0.0.1:27017/",
+    mongoURI: "mongodb://127.0.0.1:27017/",
 
     // localhost
     // mongoURI: "mongodb://127.0.0.1:27017/musictherapy",
 
     // heroku
-    mongoURI: "mongodb+srv://fenomen:fktif228@cluster0.3qpb1.mongodb.net/musictherapy?retryWrites=true&w=majority",
+    // mongoURI: "mongodb+srv://fenomen:fktif228@cluster0.3qpb1.mongodb.net/musictherapy?retryWrites=true&w=majority",
     jwt: 'dev-jwt'
 }
