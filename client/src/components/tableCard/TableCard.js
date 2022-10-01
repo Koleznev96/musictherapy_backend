91c054c32ba5ce742d089317f4a2e4a1d78eaa55
Aleksey Koleznev <fc.fenomen@mail.ru>
2022-10-01 11:54:26 +0700

    debag

--- client/src/components/tableCard/TableCard.js
+++ client/src/components/tableCard/TableCard.js
@@ -136,6 +136,7 @@ export const TableCard = ({option, data, loading, reload, setData, optionQuestio
     const langsStringTo = (strings) => {
         let new_string = [];
         for(let i = 0; i < strings.length; i++) {
+            console.log('naaaa-', strings[i], checkLanguageConst(strings[i], auth.translations))
             new_string.push(checkLanguageConst(strings[i], auth.translations));
         }
         return new_string.join(', ');
--- src/api/admin/authAdmin.js
+++ src/api/admin/authAdmin.js
@@ -3,7 +3,7 @@ const User = require('../../models/User');
 
 module.exports.check = async function(req, res) {
     let candidate = await User.findOne({token: req.headers['authorization']});
-    if (candidate && candidate.is_admin) {
+    if (candidate && (candidate.type_admin === 'Администратор' || candidate.type_admin === 'Музыкотерапевт')) {
         return {id: candidate._id, candidate};
     } else {
         return {error: 'error'};
